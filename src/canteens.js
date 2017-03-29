2a429dae374127523c138ce227a22d0455802355
Max Muth <muth.maxi@gmail.com>
2017-03-29 22:20:51 +0200

    formatting in the canteens.js

--- src/canteens.js
+++ src/canteens.js
@@ -4,28 +4,54 @@ module.exports = [
     {
         "verboseName": "Mensa Garching",
         "synonyms": [
-            "mensa garching", "mensa in garching", "garching", "garching forschungszentrum", "mensa garching forschungszentrum", "garchinger mensa"
+            "mensa garching",
+            "mensa in garching",
+            "garching",
+            "garching forschungszentrum",
+            "mensa garching forschungszentrum",
+            "garchinger mensa"
         ],
         "id": 139
     },
     {
         "verboseName": "Mensa Leopoldstraße",
-        "synonyms": ["mensa leopoldstraße", "leopoldstraße", "mensa in der leopoldstraße", "mensa an der leopoldstraße"],
+        "synonyms": [
+            "mensa leopoldstraße",
+            "leopoldstraße",
+            "mensa in der leopoldstraße",
+            "mensa an der leopoldstraße"
+        ],
         "id": 133
     },
     {
         "verboseName": "Mensa Arcisstraße",
-        "synonyms": ["mensa arcisstraße", "arcisstraße", "stammgelände", "mensa stammgelände", "mensa am stammgelände", "mensa in der arcisstraße", "mensa bei der arcisstraße"],
+        "synonyms": [
+            "mensa arcisstraße",
+            "arcisstraße",
+            "stammgelände",
+            "mensa stammgelände",
+            "mensa am stammgelände",
+            "mensa in der arcisstraße",
+            "mensa bei der arcisstraße"],
         "id": 138
     },
     {
         "verboseName": "Mensa Martinsried",
-        "synonyms": ["mensa martinsried", "martinsried", "mensa in martinsried"],
+        "synonyms": [
+            "mensa martinsried",
+            "martinsried",
+            "mensa in martinsried"
+        ],
         "id": 134
     },
     {
         "verboseName": "Mensa Großhadern",
-        "synonyms": ["mensa großhadern", "großhadern", "mensa in großhadern", "mensa bei großhadern"],
+        "synonyms": [
+            "mensa großhadern",
+            "großhadern",
+            "mensa in großhadern",
+            "mensa bei großhadern"
+        ],
         "id": 135
     },
 ];
